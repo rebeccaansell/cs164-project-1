a3c561dd9a6bd04d788d12b07ba81e70fd26e73c
rebeccaansell <rebecca.j.ansell@gmail.com>
2023-03-19 21:04:16 -0400

    Initial commit

---
+++ src/components/logout-button.js
@@ -0,0 +1,21 @@
+// src/components/logout-button.js
+
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button } from 'react-bootstrap';
+
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+  return (
+    <Button
+      className="btn-margin"
+      onClick={() =>logout()}
+      id = "qs.LogoutBtn"
+      variant = "danger"
+    >
+      Log Out
+    </Button>
+  );
+};
+
+export default LogoutButton;
--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavLink as RouterNavLink } from "react-router-dom";
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { useAuth0 } from './auth/auth0-react';
+import LoginButton from "./login-button";
+import LogoutButton from "./logout-button";
 
 const MainNav = () => (
   <Nav className="mr-auto">
@@ -31,7 +34,15 @@ const MainNav = () => (
   </Nav>
 );
 
-const AuthNav = () => null;
+const AuthNav = () => {
+  const {isAuthenticated } = useAuth0();
+
+  return (
+    <Nav className = "justify-content-end">
+      {isAuthenticated ? <LogoutButton /> : <LoginButton />}
+    </Nav>
+  )
+}
 
 const NavBar = () => {
   return (
